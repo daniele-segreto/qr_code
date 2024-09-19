66c9ed31a0503b20d0b75bcf134a61afe1f2c9ce
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-09-19 16:39:27 +0200

    Modificato il l’indirizzo, il path a cui punta il QR Code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Testo o URL che desideri convertire in codice QR
-let testoCodiceQR = "https://www.example.com";
+let testoCodiceQR = "https://www.arduino.cc/";
 
 // Crea un nuovo oggetto QRCode
 let qrcode = new QRCode(document.getElementById("qrcode"), {
